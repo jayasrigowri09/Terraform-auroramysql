419f81b60fd98d15710ef999fd3f6876dd733a4b
jayasrigowri09 <jayasrigowri.selvaraj@ust.com>
2023-03-23 22:43:13 -0400

    add

---
+++ rds-mysqlaurora/main.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+    region  = "us-east-1"
+    version = "~> 4.59.0"
+}
+
+resource "aws_rds_cluster" "example_cluster" {
+    engine = var.engine
+    engine_mode = var.engine_mode
+    #family = "aurora-mysql5.7"
+    engine_version = var.engine_version
+    cluster_identifier = var.rds_cluster_identifier
+    master_username = var.master_username
+    master_password = var.master_password
+    database_name = var.database_name
+    port = var.port
+    #skip_final_snapshot = true
+    iam_database_authentication_enabled = var.iam_database_authentication_enabled
+    deletion_protection = var.deletion_protection
+    db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+    vpc_security_group_ids = [aws_security_group.database_security_group.id]
+ }
+ 
+resource "aws_rds_cluster_instance" "example" {
+    identifier = "${var.rds_cluster_identifier}-example-instance"
+    cluster_identifier = join("", aws_rds_cluster.example_cluster.*.id)
+    instance_class = var.instance_class
+    publicly_accessible = var.publicly_accessible
+    engine = aws_rds_cluster.example_cluster.engine
+    engine_version = aws_rds_cluster.example_cluster.engine_version
+    db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+    tags = {
+        Name = "demo-mysql-aurora"
+    }
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" { 
+    name = "db-subnet"
+    subnet_ids = [aws_subnet.private-subnet-3.id, aws_subnet.private-subnet-4.id] 
+    description = "subnet for database instance"
+    tags = {
+        Name = "db-subnet"
+    }
+}
+
+resource "aws_security_group" "database_security_group" {
+    name = "Database Security Group"
+    description = "Enable Amazon Aurora Mysql access"
+    vpc_id = aws_vpc.vpc.id
+    ingress {
+        description = "MYSQL/Aurora access"
+        protocol = "tcp"
+        from_port = 3306
+        to_port = 3306
+    }
+    egress {
+        from_port = 0 
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        
+    }
+     tags = {
+            Name = "Db-security-group"
+        }
+
+}
